e19b3e518b7fdbd62d83d90a9a905ed6b7159342
lesatdgl <les@dunawaygroupllc.com>
2021-07-16 15:36:12 -0400

    Added contacts, about and their routes

--- Website1/app.py
+++ Website1/app.py
@@ -3,10 +3,20 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/About')
+def about():
+    return render_template('about.html')
+
+@app.route('/Contact')
+def contact():
+    return render_template('contact.html')
+
+
 
